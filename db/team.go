9ed90427fe3f10327c6d2bc524be11da8ae30a72
Yash <yashraj25.yr@gmail.com>
2025-06-21 21:11:29 +0530

    Fix Error Handling and Error Parsing

--- db/team.go
+++ db/team.go
@@ -33,7 +33,7 @@ func SaveTeamConfig(team TeamConfig) error {
 func GetAllTeamConfigs() ([]TeamConfig, error) {
 	var teams []TeamConfig
 	err := DB.Find(&teams).Error
-	if err != nil {
+	if err == nil {
 		for _, team := range teams {
 			team.AccessToken, _ = utils.Decrypt(team.AccessToken)
 		}
